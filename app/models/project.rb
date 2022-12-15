ca7d4a454905eeb143131fe233b9e9597e3fc866
0mny5 <adel_chrono_48@yahoo.co.jp>
2022-12-15 22:49:02 +0900

    Fix: 曲数バリデーションをupdateにも適用

--- app/models/project.rb
+++ app/models/project.rb
@@ -8,7 +8,7 @@ class Project < ApplicationRecord
   before_create -> { self.uuid = SecureRandom.uuid }
 
   validates :title, presence: true
-  validate :analyze_cannot_be_less_than_two, on: :create
+  validate :analyze_cannot_be_less_than_two
 
   private
 
