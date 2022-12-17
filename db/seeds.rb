b60f3158e00b0f37cee858e09c88381e35108ae8
0mny5 <adel_chrono_48@yahoo.co.jp>
2022-12-17 22:50:19 +0900

    Add: adminを追加するseed作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+AdminUser.create!(
+  name: ENV['ADMIN_NAME'],
+  password: ENV['ADMIN_SECRET']
+)
